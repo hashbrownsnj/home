a38c690f4bd54f1fbca357eb3d78a56e88c8809b
Aakshat Hariharan <163146641+Aaks-hatH@users.noreply.github.com>
2026-06-23 14:57:15 -0400

    Update Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -138,7 +138,6 @@ export default function Team() {
           </div>
         </div>
       </motion.div>
-      <MemberShowcase prefersReducedMotion={prefersReducedMotion} />
     </section>
   )
 }
